bc10506a3cad22004bbdcced4583f296c9568add
Ariane <ariane.ernandorena@nexusedge.co>
2020-06-02 10:56:10 -0500

    finished up components layout and styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
 import React from 'react';
 
 import Navigation from "./components/navigation/navigation";
+import Logo from "./components/logo/logo";
+import ImageLinkForm from "./components/imageLinkForm/imageLinkForm";
+import Rank from "./components/rank/rank";
+import Particles from "react-particles-js";
 
 import './App.css';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area:800,
+      }
+    }
+  },
+  interactivity: {
+    events: {
+      "onhover": {
+        enable: true,
+        mode: "repulse",
+      }
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
+      <Particles className="particles"
+              params={particlesOptions}
+            />
       <Navigation />
-      {/* <Logo />
+      <Logo />
       <ImageLinkForm />
-      <FaceRecognition /> */}
+      <Rank />
+      {/* <FaceRecognition /> */}
     </div>
   );
 }
